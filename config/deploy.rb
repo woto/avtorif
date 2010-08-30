2fd88349f0a1c2d945182f578cb96486d22f3a75
Руслан Корнев <oganer@gmail.com>
2010-08-30 17:02:40 +0400

    Немного поработал с деплоем

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,30 +4,33 @@ set :repository,  "git://github.com/woto/avtorif.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-#role :app, "your app-server here"                          # This may be the same as your `Web` server
-#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
-
 server "192.168.1.103", :app, :web, :db, :primary => true
 
+set :use_sudo, true
 default_run_options[:pty] = true
+set :user, "deploy"
 
 require 'rubygems'
 
 require 'cap_recipes/tasks/delayed_job'
-#set :delayed_script_path, 'script/djworker' # defaults to 'script/delayed_job'
-#set :delayed_job_env, 'staging' # defaults to production
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+# bundler
+namespace :bundler do
+  task :bundle_install do
+    run("cd #{release_path} && bundle install")
+  end
+end
+
+after 'deploy:update_code', 'bundler:bundle_install'
+#after 'deploy:update', 'bundler:bundle_install'
+
 
 require 'cap_recipes/tasks/rails'
 require 'cap_recipes/tasks/ruby'
 require 'cap_recipes/tasks/rubygems'
 require 'cap_recipes/tasks/apache'
 require 'cap_recipes/tasks/whenever'
+
 
 # namespace :deploy do
 #   task :start do ; end
