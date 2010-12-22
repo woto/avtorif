8054ce7ab746fafe4eb110d0a7845e4761ecf4ca
Руслан Корнев <oganer@gmail.com>
2010-12-22 15:32:51 +0300

    Вроде переделал xls.py, старый переименовал, была проблема с конвертированием чисел (напр. кат. ном. 2102 в результате получался как 2102.0)

--- system/external_tools/py_xls2csv.py
+++ system/external_tools/py_xls2csv.py
@@ -1,15 +1,104 @@
 #!/usr/bin/env python
-# -*- coding: windows-1251 -*-
-# Copyright (C) 2005 Kiseliov Roman
+# -*- coding: utf-8 -*- 
 
-__rev_id__ = """$Id: xls2csv.py,v 1.1 2005/10/26 07:44:24 rvk Exp $"""
+class readexcel(object):
+    """ Simple OS-independent class for extracting data from an Excel File.
+    
+    Uses the xlrd module (version 0.5.2 or later), supporting Excel versions:
+    2004, 2002, XP, 2000, 97, 95, 5, 4, 3
+    
+    Data is extracted via iterators that return one row at a time -- either 
+    as a dict or as a list. The dict generator assumes that the worksheet is 
+    in tabular format with the first "data" row containing the variable names
+    and all subsequent rows containing values. 
+        
+    Extracted data is represented fairly logically. By default dates are 
+    returned as strings in "yyyy/mm/dd" format or "yyyy/mm/dd hh:mm:ss", as 
+    appropriate. However, when specifying date_as_tuple=True, dates will be 
+    returned as a (Year, Month, Day, Hour, Min, Second) tuple, for usage with 
+    mxDateTime or DateTime. Numbers are returned as either INT or FLOAT, 
+    whichever is needed to support the data. Text, booleans, and error codes 
+    are also returned as appropriate representations. Quick Example:
+    
+        xls = readexcel('testdata.xls')
+        for sname in xls.book.sheet_names():
+            for row in xls.iter_dict(sname):
+                print row
+    """ 
+    def __init__(self, filename, enc):
+        """ Wraps an XLRD book """
+        if not os.path.isfile(filename):
+            raise ValueError, "%s is not a valid filename" % filename
+        self.book = xlrd.open_workbook(filename, encoding_override=enc)
+        self.sheet_keys = {}
+    def is_data_row(self, sheet, i):
+        values = sheet.row_values(i)
+        if isinstance(values[0], basestring) and values[0].startswith('#'):
+            return False # ignorable comment row
+        for v in values:
+            if bool(v):
+                return True #+ row full of (valid) False values?
+        return False
+    def _parse_row(self, sheet, row_index, date_as_tuple):
+        """ Sanitize incoming excel data """
+        # Data Type Codes:
+        #  EMPTY 0
+        #  TEXT 1 a Unicode string 
+        #  NUMBER 2 float 
+        #  DATE 3 float 
+        #  BOOLEAN 4 int; 1 means TRUE, 0 means FALSE 
+        #  ERROR 5 
+        values = []
+        for type, value in zip(
+                sheet.row_types(row_index), sheet.row_values(row_index)):
+            if type == 2:
+                if value == int(value):
+                    value = int(value)
+            elif type == 3:
+                datetuple = xlrd.xldate_as_tuple(value, self.book.datemode)
+                if date_as_tuple:
+                    value = datetuple
+                else:
+                    # time only no date component
+                    if datetuple[0] == 0 and datetuple[1] == 0 and \
+                       datetuple[2] == 0: 
+                        value = "%02d:%02d:%02d" % datetuple[3:]
+                    # date only, no time
+                    elif datetuple[3] == 0 and datetuple[4] == 0 and \
+                         datetuple[5] == 0:
+                        value = "%04d/%02d/%02d" % datetuple[:3]
+                    else: # full date
+                        value = "%04d/%02d/%02d %02d:%02d:%02d" % datetuple
+            elif type == 5:
+                value = xlrd.error_text_from_code[value]
+            
+            #pdb.set_trace()
+            if isinstance(value, unicode):
+                value = value.encode('utf8')
+            values.append(value)
+        return values
+    def iter_dict(self, sname, date_as_tuple=False):
+        """ Iterator for the worksheet's rows as dicts """
+        sheet = self.book.sheet_by_name(sname) # XLRDError
+        # parse first row, set dict keys & first_row_index
+        keys = []
+        first_row_index = 0
+        self.sheet_keys[sname] = keys
+        # generate a dict per data row 
+        if first_row_index is not None:
+            for i in range(first_row_index, sheet.nrows):
+                if self.is_data_row(sheet, i): 
+                    yield self._parse_row(sheet, i, date_as_tuple)
+    def iter_list(self, sname, date_as_tuple=False):
+        """ Iterator for the worksheet's rows as lists """
+        sheet = self.book.sheet_by_name(sname) # XLRDError
+        for i in range(sheet.nrows):
+            if self.is_data_row(sheet, i): 
+                yield self._parse_row(sheet, i, date_as_tuple)
 
-
-from pyExcelerator import *
 from optparse import OptionParser
-import sys
-import csv
-import pdb
+import csv, xlrd, re, decimal, os, pdb
+
 parser = OptionParser()
 parser.add_option("-i", "--input-file", dest="input_file", help="input file name to parse", metavar="INPUT")
 parser.add_option("-t", "--temp-path", dest="temp_path", help="temp path", metavar="TEMP_PATH")
@@ -23,54 +112,10 @@ temp_path = options.temp_path
 encoding = options.encoding
 arg = options.input_file
 
-#if args:
-#    for arg in args:
-        #print >>sys.stderr, 'extracting data from', arg
-for sheet_name, values in parse_xls(arg, str(encoding)): # parse_xls(arg) -- default encoding
-    matrix = [[None]]
-
-    #print 'Sheet = "%s"' % sheet_name.encode('cp866', 'backslashreplace')
-    #print '----------------'
-
+xls = readexcel(arg, encoding)
+for sheet_name in xls.book.sheet_names():
     sheet_file = csv.writer(open(temp_path + "/" + sheet_name.encode('UTF-8', 'backslashreplace'), 'wb'), delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#    once = True
-#    first_idx = 0
-
-    for row_idx, col_idx in sorted(values.keys()):
-#        if(once):
-#            once = False
-#            first_idx = col_idx
-#        pdb.set_trace()
-        v = values[(row_idx, col_idx)]
-        #pdb.set_trace()
-        if isinstance(v, unicode):
-            v = v.encode(str(encoding), 'backslashreplace')
-        else:
-            v = `v`
-        v = '%s' % v.strip()
-
-        try:
-            v = int(v)
-        except:
-            0
-
-        last_row, last_col = len(matrix), len(matrix[-1])
-        while last_row <= row_idx:
-            matrix.extend([[]])
-            last_row = len(matrix)
-            last_col = 0
-
-        while last_col < col_idx:
-            matrix[-1].extend([''])
-            last_col = len(matrix[-1])
-
-        matrix[-1].extend([v])
-            
-    for row in matrix:
-        #csv_row = ', '.join(row)
+    #pdb.set_trace()
+    for row in xls.iter_dict(sheet_name):
         sheet_file.writerow(row)
-    #sheet_file.close
-
-#else:
-#print 'usage: %s (inputfile)+' % me
 
