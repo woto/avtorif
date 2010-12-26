c0953c50219885a4a93fd29b817bd9f401c375a0
Корнев Руслан <oganer@gmail.com>
2010-12-26 03:18:40 +0300

    Теперь путь до файла для http_receive тоже должен быть в кавычках, т.к. добавлен метод eval

--- lib/http_receiver.rb
+++ lib/http_receiver.rb
@@ -15,7 +15,7 @@ class HttpReceiver < AbstractReceiver
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       end
 
-      request = Net::HTTP::Get.new(URI::escape(@receiver.path + @job.file_mask))
+      request = Net::HTTP::Get.new(URI::escape(@receiver.path + eval(@job.file_mask)))
       request.initialize_http_header({"User-Agent" => "avtorif"})
 
       response = http.request(request)
