56081455d5be3a70a11c874ae2d635942f197096
Ruslan Kornev <oganer@gmail.com>
2011-05-02 21:53:19 +0400

    Включил логгирование для DJ, сейчас запушу на сервер

--- lib/abstract_jobber.rb
+++ lib/abstract_jobber.rb
@@ -1,48 +1,45 @@
-#logger = Rails::logger
 #logger.level = Logger::ERROR
 
 class AbstractJobber
-
   attr_accessor :optional, :job, :jobable, :priority
 
   def enqueue(j)
     j.job_id = job.id
     j.save
-    puts Time.zone.now.to_s + " Безусловный enqueue: '#{job.title}' от '#{job.supplier.title}'."
     job.locked = true
     job.save
+    Rails.logger.info(Time.zone.now.to_s + " Безусловный enqueue: '#{job.title}' от '#{job.supplier.title}'.")    
   end
 
   def before(j)
-    puts Time.zone.now.to_s + " Безусловный before: '#{job.title}' от  '#{job.supplier.title}'."
     job.last_start = Time.zone.now.to_s
     job.started_once = true
     job.save
+    Rails.logger.info(Time.zone.now.to_s + " Безусловный before: '#{job.title}' от  '#{job.supplier.title}'.")    
   end
 
   def after(j)
-    puts Time.zone.now.to_s + " Безусловный after: '#{job.title}' от '#{job.supplier.title}'."
     job.last_error = ''
+    Rails.logger.info(Time.zone.now.to_s + " Безусловный after: '#{job.title}' от '#{job.supplier.title}'.")    
   end
 
   def success(j)
-    #sleep(5)
     job.locked = false
     job.last_finish = Time.zone.now.to_s
     job.last_error = ''
     job.save
-    puts Time.zone.now.to_s + " Условный success: '#{job.title}' от '#{job.supplier.title}'."
+    Rails.logger.info(Time.zone.now.to_s + " Условный success: '#{job.title}' от '#{job.supplier.title}'.")
   end
 
   def error(j, e)
     job.last_error = e.message
     job.save
-    puts Time.zone.now.to_s + " Условный error: '#{job.title}' от '#{job.supplier.title}'."
-    puts e.message
+    Rails.logger.info(Time.zone.now.to_s + " Условный error: '#{job.title}' от '#{job.supplier.title}'.")
+    Rails.logger.info(e.message)
   end
 
   def failure
-    puts Time.zone.now.to_s + " Условный failure: '#{job.title}' от '#{job.supplier.title}'."
+    Rails.logger.info(Time.zone.now.to_s + " Условный failure: '#{job.title}' от '#{job.supplier.title}'.")
   end
 
   def initialize(job, jobable, priority, optional = nil)
